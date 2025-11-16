1283981bc14e9f166a79b4529a0e89083e96beec
Aizhan Dussanova <aizhan_dussanova@epam.com>
2025-11-16 16:11:30 +0500

    PyTest Homework

--- PyTest Introduction/tests/conftest.py
+++ PyTest Introduction/tests/conftest.py
@@ -1,10 +1,37 @@
+import os
+
 import pytest
 import pandas as pd
 
 # Fixture to read the CSV file
+@pytest.fixture(scope = "session")
+def file():
+    conftest_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(conftest_dir, "../src/data/data.csv")
+    absolute_csv_path = os.path.abspath(csv_path)
+    df = pd.read_csv(absolute_csv_path)
+    return df
 
 
 # Fixture to validate the schema of the file
+@pytest.fixture(scope="session")
+def schema(file):
+    actual = list(file.columns)
+    expected = ["id", "name", "age", "email"]
+    return actual, expected
+
+
+@pytest.fixture(scope = "session")
+def players_data():
+    return [(2, True)]
+
+@pytest.fixture(scope = "session")
+def list_of_dict(file):
+    return file.to_dict(orient="records")
 
 
 # Pytest hook to mark unmarked tests with a custom mark
+def pytest_collection_modifyitems(config, items):
+    for item in items:
+        if not item.own_markers:
+            item.add_marker("unmarked")
--- PyTest Introduction/tests/test_csv/test_csv_file.py
+++ PyTest Introduction/tests/test_csv/test_csv_file.py
@@ -1,30 +1,81 @@
 import pytest
 import re
+"""
+1. Write tests to validate the content of the CSV file:
+Validate that file is not empty.
+Validate the schema of the file (id, name, age, email).
+Validate that the age column contains valid values (0-100).
+Validate that the email column contains valid email addresses (format).
+Validate there are no duplicate rows.
+Validate that:is_active=False for id=1, is_active=True for id=2.
+Same as previous one for id = 2, but without parametriz mark. 
+•	Use custom error messages in assertions.
+
+2. Use PyTest Hooks:
+•	Implement a hook in conftest.py to dynamically mark tests that do not have explicit marks. The hook should assign tests without marks to a custom mark:unmarked.
+
+3. Create fixtures in conftest.py:
+•	A fixture to read the CSV file and return its content. Parameters: path_to_file.
+•	A fixture to validate the schema of the file. Parameters: actual_schema, expected_schema.
+•	Fixtures scope: session.
+
+4. Use Custom and Predefined Marks:
+•	Use predefined marks @pytest.mark.parametrize, @pytest.mark.skip, @pytest.mark.xfail and create custom marks.
+
+5. Populate the pytest.ini file with custom marks and modify test discovery settings to only look for files prefixed with test_.
+6. Configure PyTest to generate an HTML report upon test execution.
+"""
+
+def test_file_not_empty(file):#1                                            ready
+    #print(file)
+    assert not file.empty, "The CSV file must not be empty"
 
 
-def test_file_not_empty():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+@pytest.mark.xfail(reason="xfail")
+def test_duplicates(list_of_dict):#5                                        ready
+    list_as_tuples = [tuple(d.items()) for d in list_of_dict]
+    unique_items = set(list_as_tuples)
+    assert len(unique_items) == len(list_of_dict), "Duplicate rows detected in the CSV file"
 
 
-def test_duplicates():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+def test_validate_schema(list_of_dict, schema):#2                           ready
+    actual, expected = schema
+    assert actual == expected, f"Schema mismatch"
 
 
-def test_validate_schema():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+@pytest.mark.skip(reason="skip")
+def test_age_column_valid(list_of_dict):#3                                  ready
+    for record in list_of_dict:
+        age = record.get("age")
+        assert 0 <= age <= 100
 
 
-def test_age_column_valid():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+def test_email_column_valid(list_of_dict):#4                                ready
+    for record in list_of_dict:
+        email = record.get("email")
+        #print(email)
+        pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        assert re.fullmatch(pattern, email) is not None, f"Invalid email format: {email}"
 
 
-def test_email_column_valid():
-    assert 1 + 1 == 2
+@pytest.mark.parametrize("id, is_active", [(1, False), (2, True)])
+def test_active_players(list_of_dict, id, is_active):#6                     ready
+    assert active_payer_check(list_of_dict, id, is_active), f"Incorrect 'is_active' value for id={id}. Expected {is_active}."
 
 
-def test_active_players():
-    assert 1 + 1 == 2
+def test_active_player(list_of_dict, players_data):#7                       ready
+    for params in players_data:
+        id, is_active = params
+        assert active_payer_check(list_of_dict, id, is_active), f"Incorrect 'is_active' value for id={id}. Expected {is_active}."
 
 
-def test_active_player():
-    assert 1 + 1 == 2
+def active_payer_check(list, id, is_active):
+    for record in list:
+        record_id = record.get("id")
+        record_is_active = record.get("is_active")
+        if record_id == id:
+            return record_is_active == is_active
