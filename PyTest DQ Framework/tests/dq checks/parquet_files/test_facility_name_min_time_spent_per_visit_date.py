2e6d798984e78c21d5db32d0fde4237b9b84bdcf
Aizhan Dussanova <aizhan_dussanova@epam.com>
2025-11-25 17:25:55 +0500

    The Update pipeline editing

--- PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
+++ PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
@@ -19,7 +19,7 @@ JOIN facilities f ON f.id = v.facility_id
 GROUP BY
     f.facility_name,
     visit_date
---ORDER BY f.facility_name, visit_date
+ORDER BY f.facility_name, visit_date
 ;
 """
     source_data = db_connection.get_data_sql(source_query)
